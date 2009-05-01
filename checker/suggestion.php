736a2e438a5df1f159e1c3a1a3b9a9b3c5c9a681
Cindy Li <cli@ocad.ca>
2009-05-01 13:37:05 +0000

    1. fix html mark up language and accessibility errors on suggestion page 2. remove not-in-use styles from styles.css

--- checker/suggestion.php
+++ checker/suggestion.php
@@ -50,13 +50,12 @@ $guidelineSubgroupsDAO = new GuidelineSubgroupsDAO();
 ?>
 <div class="output-form">
 	
-<h2><? echo _AC("html_tag"); ?></h2>
+<h2><?php echo _AC("html_tag"); ?></h2>
 <span class="msg"><?php echo $row["html_tag"]; ?></span>
 
 <?php if (is_array($guideline_rows)) {?> 
-<h2><? echo _AC("guidelines"); ?></h2>
-<span class="msg">
-	<ul>
+<h2><?php echo _AC("guidelines"); ?></h2>
+	<ul class="msg">
 <?php 	foreach ($guideline_rows as $guideline) {?>
 		<li>
 			<a title="<?php echo $guideline['title']._AC('link_open_in_new'); ?>" target="_new" href="<?php echo AC_BASE_HREF; ?>guideline/view_guideline.php?id=<?php echo $guideline['guideline_id']; ?>"><?php echo $guideline["title"]; ?></a><br/>
@@ -64,24 +63,23 @@ $guidelineSubgroupsDAO = new GuidelineSubgroupsDAO();
 			$groups = $guidelineGroupsDAO->getGroupByCheckIDAndGuidelineID($check_id, $guideline['guideline_id']);
 			if (is_array($groups) && $groups[0]['name'] <> '') {
 ?>
-			<span><img src="themes/<?php echo $_SESSION['prefs']['PREF_THEME']; ?>/images/arrow.gif"><?php echo _AC($groups[0]['name']); ?></span><br/>
+			<span><img src="themes/<?php echo $_SESSION['prefs']['PREF_THEME']; ?>/images/arrow.gif" alt="<?php echo _AC("guideline_group");?>" /><?php echo _AC($groups[0]['name']); ?></span><br/>
 <?php 
 			}
 			$subgroups = $guidelineSubgroupsDAO->getSubgroupByCheckIDAndGuidelineID($check_id, $guideline['guideline_id']);
 			if (is_array($subgroups) && $subgroups[0]['name'] <> '') {
 ?>
-			<span class="padding_left"><img src="themes/<?php echo $_SESSION['prefs']['PREF_THEME']; ?>/images/arrow.gif"><?php echo _AC($subgroups[0]['name']); ?></span>
+			<span class="padding_left"><img src="themes/<?php echo $_SESSION['prefs']['PREF_THEME']; ?>/images/arrow.gif" alt="<?php echo _AC("guideline_subgroup");?>" /><?php echo _AC($subgroups[0]['name']); ?></span>
 <?php 		}?>
 		</li>
 <?php } // end of foreach?>
 	</ul>
-</span>
 <?php } // end of if?>
 
-<h2><? echo _AC("requirement"); ?></h2>
+<h2><?php echo _AC("requirement"); ?></h2>
 <span class="msg"><?php echo _AC($row["name"]); ?></span>
 
-<h2><? echo _AC("error"); ?></h2>
+<h2><?php echo _AC("error"); ?></h2>
 <span class="msg"><?php echo _AC($row["err"]); ?></span>
 
 <?php
@@ -89,7 +87,7 @@ if ($row["description"] <> "")
 {
 ?>
 
-<h2><? echo _AC("short_desc"); ?></h2>
+<h2><?php echo _AC("short_desc"); ?></h2>
 <span class="msg"><?php echo _AC($row["description"]); ?></span>
 
 <?php
@@ -101,7 +99,7 @@ if ($row["long_description"] <> "")
 {
 ?>
 
-<h2><? echo _AC("long_desc"); ?></h2>
+<h2><?php echo _AC("long_desc"); ?></h2>
 <span class="msg"><?php echo _AC($row["long_description"]); ?></span>
 
 <?php
@@ -113,7 +111,7 @@ if ($row["rationale"] <> "")
 {
 ?>
 
-<h2><? echo _AC("rationale"); ?></h2>
+<h2><?php echo _AC("rationale"); ?></h2>
 <span class="msg"><?php echo _AC($row["rationale"]); ?></span>
 
 <?php
@@ -125,7 +123,7 @@ if ($row["how_to_repair"] <> "")
 {
 ?>
 
-<h2><? echo _AC("how_to_repair"); ?></h2>
+<h2><?php echo _AC("how_to_repair"); ?></h2>
 <span class="msg"><?php echo _AC($row["how_to_repair"]); ?></span>
 
 <?php
@@ -137,8 +135,8 @@ if ($row["repair_example"] <> "")
 {
 ?>
 
-<h2><? echo _AC("repair_example"); ?></h2>
-<span class="msg"><pre><?php echo htmlentities(_AC($row["repair_example"])); ?></pre></span>
+<h2><?php echo _AC("repair_example"); ?></h2>
+<pre class="code"><?php echo htmlentities(_AC($row["repair_example"])); ?></pre>
 
 <?php
 }
@@ -149,18 +147,18 @@ if ($row["question"] <> "")
 {
 ?>
 
-<h2><? echo _AC("how_to_determine"); ?></h2>
+<h2><?php echo _AC("how_to_determine"); ?></h2>
 <table>
 	<tr>
-		<th align="left"><? echo _AC("question"); ?></th>
+		<th align="left"><?php echo _AC("question"); ?></th>
 		<td><span class="msg"><?php echo _AC($row["question"]); ?></span></td>
 	</tr>
 	<tr>
-		<th align="left"><? echo _AC("pass"); ?></th>
+		<th align="left"><?php echo _AC("pass"); ?></th>
 		<td><span class="msg"><?php echo _AC($row["decision_pass"]); ?></span></td>
 	</tr>
 	<tr>
-		<th align="left"><? echo _AC("fail"); ?></th>
+		<th align="left"><?php echo _AC("fail"); ?></th>
 		<td><span class="msg"><?php echo _AC($row["decision_fail"]); ?></span></td>
 	</tr>
 </table>
@@ -172,8 +170,8 @@ if ($row["test_procedure"] <> "")
 {
 ?>
 
-<h2><? echo _AC("steps_to_check"); ?></h2>
-	<h3><? echo _AC("procedure"); ?></h3>
+<h2><?php echo _AC("steps_to_check"); ?></h2>
+	<h3><?php echo _AC("procedure"); ?></h3>
 	<span class="msg"><?php echo Utility::convertHTMLNewLine(_AC($row["test_procedure"])); ?></span><br />
 <?php
 }
@@ -182,7 +180,7 @@ if ($row["test_expected_result"] <> "")
 {
 ?>
 
-	<h3><? echo _AC("expected_result"); ?></h3>
+	<h3><?php echo _AC("expected_result"); ?></h3>
 	<span class="msg"><?php echo Utility::convertHTMLNewLine(_AC($row["test_expected_result"])); ?></span><br />
 <?php
 }
@@ -191,36 +189,28 @@ if ($row["test_failed_result"] <> "")
 {
 ?>
 
-	<h3><? echo _AC("failed_result"); ?></h3>
+	<h3><?php echo _AC("failed_result"); ?></h3>
 	<span class="msg"><?php echo Utility::convertHTMLNewLine(_AC($row["test_failed_result"])); ?></span><br />
 <?php
 }
 ?>
 
 <?php if (is_array($pass_examples) || is_array($fail_examples)) {?>
-<h2><? echo _AC("examples"); ?></h2>
+<h2><?php echo _AC("examples"); ?></h2>
 
 <?php 	if (is_array($pass_examples)) {?>
-<h3><? echo _AC("pass_examples"); ?></h3>
+<h3><?php echo _AC("pass_examples"); ?></h3>
 <?php 		foreach ($pass_examples as $pass_example) {?>
-<span class="msg">
-<?php echo $pass_example['description']; ?><br/>
-<pre class="code">
-<?php echo htmlentities($pass_example['content']); ?>
-</pre>
-</span>
+<span class="msg"><?php echo $pass_example['description']; ?></span>
+<pre class="code"><?php echo htmlentities($pass_example['content']); ?></pre>
 <?php 		} // end of foreach?>
 <?php 	} // end of if (pass examples)?>
 
 <?php 	if (is_array($fail_examples)) {?>
-<h3><? echo _AC("fail_examples"); ?></h3>
+<h3><?php echo _AC("fail_examples"); ?></h3>
 <?php 		foreach ($fail_examples as $fail_example) {?>
-<span class="msg">
-<?php echo $fail_example['description']; ?><br/>
-<pre class="code">
-<?php echo htmlentities($fail_example['content']); ?>
-</pre>
-</span>
+<span class="msg"><?php echo $fail_example['description']; ?></span>
+<pre class="code"><?php echo htmlentities($fail_example['content']); ?></pre>
 <?php 		} // end of foreach?>
 <?php 	} // end of if (pass examples)?>
 
