71c7a385f25ce8407c4210c12cf2f34602ed22c7
Greg Gay <info@atutor.ca>
2009-06-03 17:30:42 +0000

    change version number to 1.0

--- include/constants.inc.php
+++ include/constants.inc.php
@@ -16,7 +16,7 @@ if (!defined('AC_INCLUDE_PATH')) { exit; }
  * constants, some more constants are loaded from table 'config' @ include/vitals.inc.php
  **/
 
-define('VERSION',		'0.8');
+define('VERSION',		'1.0');
 
 // language constants
 define('DEFAULT_LANGUAGE_CODE', 'eng');
