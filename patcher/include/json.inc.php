1bb10c91ee4ab013223b455d73c0ffda7a746431
Cindy Li <cli@ocad.ca>
2009-06-22 19:45:36 +0000

    scripts for Updater

---
+++ patcher/include/json.inc.php
@@ -0,0 +1,72 @@
+<?php
+/************************************************************************/
+/* AChecker                                                             */
+/************************************************************************/
+/* Copyright (c) 2008 by Greg Gay, Cindy Li                             */
+/* Adaptive Technology Resource Centre / University of Toronto          */
+/*                                                                      */
+/* This program is free software. You can redistribute it and/or        */
+/* modify it under the terms of the GNU General Public License          */
+/* as published by the Free Software Foundation.                        */
+/************************************************************************/
+
+// my simple JSON encode/decode function, similar to PHP5.2 
+function json_encode_result($phpdata) {
+	if (!$phpdata) return "[]";
+	
+	if(is_array($phpdata)) 
+		return php2js_array($phpdata);
+	else
+		return php2js_object($phpdata);
+}
+
+
+//function json_decode($jsonata) {
+//	return $phpdata;
+//}
+
+
+// convert a PHP object to javascript object
+function php2js_object($phpobj) {
+	$str = ""; 
+	
+	if (!is_array($phpobj)) return "[]";
+	
+	foreach($phpobj as $col => $val) {
+	  if($str == "")
+	    $str = $col .":'" . escapeString($val) . "'";
+	  else
+	    $str = $str . "," . $col .":'" . escapeString($val) . "'";
+	}
+	
+	return "{" . $str . "}";
+}
+
+// convert a PHP object to javascript object
+function php2js_array($phparr) {
+	$str = "";
+
+  if (!is_array($phparr)) return "[]";
+
+	foreach ($phparr as $e) {
+	  if($str == "") 
+			$str = php2js_object($e) ;
+	  else
+	    $str = $str . "," . php2js_object($e);
+	}
+	
+	return "[" . $str . "]";
+}
+
+function escapeString($string) {
+    $escape = array(
+    "\r\n" => '\n',
+    "\r"   => '\n',
+    "\n"   => '\n',
+    "/"    => '\/'
+    );
+
+    return str_replace(array_keys($escape), array_values($escape), addslashes($string));
+}	
+
+?>
---
+++ patcher/patch_delete.php
@@ -0,0 +1,61 @@
+<?php
+/************************************************************************/
+/* AChecker                                                             */
+/************************************************************************/
+/* Copyright (c) 2008 by Greg Gay, Cindy Li                             */
+/* Adaptive Technology Resource Centre / University of Toronto          */
+/*                                                                      */
+/* This program is free software. You can redistribute it and/or        */
+/* modify it under the terms of the GNU General Public License          */
+/* as published by the Free Software Foundation.                        */
+/************************************************************************/
+
+define('AC_INCLUDE_PATH', '../include/');
+require(AC_INCLUDE_PATH.'vitals.inc.php');
+require_once(AC_INCLUDE_PATH.'classes/DAO/MyownPatchesDAO.class.php');
+require_once(AC_INCLUDE_PATH.'classes/DAO/MyownPatchesDependentDAO.class.php');
+require_once(AC_INCLUDE_PATH.'classes/DAO/MyownPatchesFilesDAO.class.php');
+
+$myownPatchesDAO = new MyownPatchesDAO();
+
+if (isset($_POST['submit_no'])) {
+	$msg->addFeedback('CANCELLED');
+	header('Location: myown_patches.php');
+	exit;
+} else if (isset($_POST['submit_yes'])) {
+	/* delete has been confirmed, delete this category */
+	$myown_patch_id	= intval($_POST['myown_patch_id']);
+
+	$myownPatchesDependentDAO = new MyownPatchesDependentDAO();
+	$myownPatchesFilesDAO = new MyownPatchesFilesDAO();
+	
+	$myownPatchesDAO->Delete($myown_patch_id);
+	$myownPatchesDependentDAO->DeleteByPatchID($myown_patch_id);
+	$myownPatchesFilesDAO->DeleteByPatchID($myown_patch_id);
+
+	$msg->addFeedback('ACTION_COMPLETED_SUCCESSFULLY');
+	header('Location: myown_patches.php');
+	exit;
+}
+
+//require('../../include/header.inc.php');
+require(AC_INCLUDE_PATH.'header.inc.php');
+
+$_GET['myown_patch_id'] = intval($_GET['myown_patch_id']); 
+$row = $myownPatchesDAO->getByID($_GET[myown_patch_id]);
+
+if (!$row) {
+	$msg->printErrors('ITEM_NOT_FOUND');
+} else {
+	$hidden_vars['achecker_patch_id']= $row['achecker_patch_id'];
+	$hidden_vars['myown_patch_id']	= $row['myown_patch_id'];
+
+	$confirm = array('DELETE_MYOWN_UPDATE', $row['achecker_patch_id']);
+	$msg->addConfirm($confirm, $hidden_vars);
+	
+	$msg->printConfirm();
+}
+
+require(AC_INCLUDE_PATH.'footer.inc.php');
+
+?>
