05818394c5d3ba1c9c159eb2d6a05cf6c1b10f11
Cindy Li <cli@ocad.ca>
2011-03-02 15:19:28 +0000

    http://atutor.ca/atutor/mantis/view.php?id=4736

--- install/db/achecker_upgrade_1.1_to_1.2.sql
+++ install/db/achecker_upgrade_1.1_to_1.2.sql
@@ -15,3 +15,40 @@ ALTER TABLE `user_decisions` DROP `sequence_id`;
 UPDATE `checks` SET `func` = 'list($width, $height) = BasicFunctions::getImageWidthAndHeight(\'src\');\r\n\r\nif (!$width)\r\n   return BasicFunctions::hasAttribute(\'longdesc\');\r\nelse\r\n   return !($width > 50 && $height > 50 && !BasicFunctions::hasAttribute(\'longdesc\'));' WHERE `check_id`=8;
 UPDATE `checks` SET `func` = 'list($width, $height) = BasicFunctions::getImageWidthAndHeight(\'src\');\r\n\r\nif (!$width)\r\n   return  false;\r\nelse\r\n   return !($width > 50 && $height > 50);' WHERE `check_id`=11;
 UPDATE `checks` SET `func` = 'list($width, $height) = BasicFunctions::getImageWidthAndHeight(\'src\');\r\n\r\nif (!$width)\r\n   return  false;\r\nelse\r\n   return !($width > 100 && $height > 100);' WHERE `check_id`=14;
+
+# --------------------------------------------------------
+# Remove duplicate checks that appear more than once in each of the guidelines
+# since 1.2
+DELETE FROM `subgroup_checks` 
+WHERE (`subgroup_id` = 1 AND `check_id` = 24)
+OR (`subgroup_id` = 1 AND `check_id` = 34)
+OR (`subgroup_id` = 1 AND `check_id` = 74)
+OR (`subgroup_id` = 1 AND `check_id` = 75)
+OR (`subgroup_id` = 1 AND `check_id` = 90)
+OR (`subgroup_id` = 1 AND `check_id` = 127)
+OR (`subgroup_id` = 62 AND `check_id` = 24)
+OR (`subgroup_id` = 62 AND `check_id` = 90)
+OR (`subgroup_id` = 80 AND `check_id` = 15)
+OR (`subgroup_id` = 92 AND `check_id` = 77)
+OR (`subgroup_id` = 100 AND `check_id` = 24)
+OR (`subgroup_id` = 100 AND `check_id` = 74)
+OR (`subgroup_id` = 100 AND `check_id` = 75)
+OR (`subgroup_id` = 100 AND `check_id` = 127)
+OR (`subgroup_id` = 117 AND `check_id` = 24)
+OR (`subgroup_id` = 117 AND `check_id` = 34)
+OR (`subgroup_id` = 117 AND `check_id` = 74)
+OR (`subgroup_id` = 117 AND `check_id` = 75)
+OR (`subgroup_id` = 117 AND `check_id` = 127)
+OR (`subgroup_id` = 162 AND `check_id` = 24)
+OR (`subgroup_id` = 162 AND `check_id` = 34)
+OR (`subgroup_id` = 162 AND `check_id` = 74)
+OR (`subgroup_id` = 162 AND `check_id` = 75)
+OR (`subgroup_id` = 162 AND `check_id` = 127)
+OR (`subgroup_id` = 163 AND `check_id` = 13)
+OR (`subgroup_id` = 224 AND `check_id` = 11)
+OR (`subgroup_id` = 234 AND `check_id` = 263)
+OR (`subgroup_id` = 250 AND `check_id` = 133)
+OR (`subgroup_id` = 278 AND `check_id` = 11)
+OR (`subgroup_id` = 287 AND `check_id` = 145)
+OR (`subgroup_id` = 349 AND `check_id` = 272)
+OR (`subgroup_id` = 351 AND `check_id` = 269);
