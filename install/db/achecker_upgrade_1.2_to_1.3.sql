a564d943482f50b0af0bda3e0484ff72842817ee
Cindy Qi Li <cli@ocad.ca>
2011-11-22 15:21:30 -0500

    Issue-4816: Updated the upgrade SQL script accordingly.

--- install/db/achecker_upgrade_1.2_to_1.3.sql
+++ install/db/achecker_upgrade_1.2_to_1.3.sql
@@ -8,3 +8,10 @@
 # since 1.3
 
 UPDATE `checks` SET `func` = 'list($width, $height) = BasicFunctions::getImageWidthAndHeight(\'src\');\r\n\r\nif (!$width)\r\n   return BasicFunctions::hasAttribute(\'longdesc\');\r\nelse\r\n   return !($width > 100 && $height > 100 && !BasicFunctions::hasAttribute(\'longdesc\'));' WHERE `check_id`=8;
+
+# ---------------------------------------------------------------------
+# Replaced check 63 with 213 in the guideline "Section 508"
+# More details at http://atutor.ca/atutor/mantis/view.php?id=4816
+# since 1.3
+
+Update `subgroup_checks` SET `check_id` = 213 WHERE `subgroup_id` = 75 AND `check_id` = 63;
