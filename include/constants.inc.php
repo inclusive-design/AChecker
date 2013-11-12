51a4175a6a0e90d0f46f5286367aef39ac18e9b0
Greg Gay <info@atutor.ca>
2013-11-12 16:08:08 -0500

    Revert "update version number"
    This reverts commit c6c70455c0a84ea9010d33910aa1b66e09c03fae.

--- include/constants.inc.php
+++ include/constants.inc.php
@@ -17,7 +17,7 @@ if (!defined('AC_INCLUDE_PATH')) { exit; }
  * constants, some more constants are loaded from table 'config' @ include/vitals.inc.php
  **/
 
-define('VERSION',	'1.4');
+define('VERSION',	'1.3');
 
 // language constants
 define('DEFAULT_LANGUAGE_CODE', 'eng');
