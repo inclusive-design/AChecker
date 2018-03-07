9d94a3226b7d07c238b6e07a2d758f707a86b8d7
pranjaljn97 <pranjal.jn97@gmail.com>
2018-03-08 02:18:39 +0530

    Updated revised_date value for term(8_min_chars) in language_text table

--- install/db/achecker_upgrade_1.3_to_1.4.sql
+++ install/db/achecker_upgrade_1.3_to_1.4.sql
@@ -14,7 +14,7 @@ ALTER TABLE `users` MODIFY `login` VARCHAR(50);
 # For More details, Visit http://www.atutor.ca/atutor/mantis/view.php?id=5846
 # since 1.3
 
-UPDATE `language_text` SET `term` = '8_min_chars',`text` = '8 minimum characters' WHERE `term`='15_max_chars';
+UPDATE `language_text` SET `term` = '8_min_chars',`text` = '8 minimum characters',`revised_date`='2018-03-08 02:00:00' WHERE `term`='15_max_chars';
 
 DELETE FROM `language_text` WHERE `term` = '20_max_chars';
 
