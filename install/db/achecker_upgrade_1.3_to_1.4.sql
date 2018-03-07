fd56457507ccd22c3e58c2489ae281a16d279dbb
pranjaljn97 <pranjal.jn97@gmail.com>
2018-03-07 22:43:24 +0530

    Formatted SQL UpgraderScript1.4

--- install/db/achecker_upgrade_1.3_to_1.4.sql
+++ install/db/achecker_upgrade_1.3_to_1.4.sql
@@ -7,14 +7,14 @@
 # For More details, Visit PR https://github.com/inclusive-design/AChecker/pull/71
 # since 1.3
 
-ALTER TABLE 'users' MODIFY login VARCHAR(50);
+ALTER TABLE `users` MODIFY login VARCHAR(50);
 
 # ---------------------------------------------------------------------
 # Data Updated and Removed from Table 'language_text'
 # For More details, Visit PR https://github.com/inclusive-design/AChecker/pull/71 
 # since 1.3
 
-UPDATE 'language_text' SET term = '8_min_chars',text = '8 minimum characters' WHERE term='15_max_chars';
+UPDATE `language_text` SET term = '8_min_chars',text = '8 minimum characters' WHERE term='15_max_chars';
 
 DELETE FROM `language_text` WHERE term = '20_max_chars';
 
