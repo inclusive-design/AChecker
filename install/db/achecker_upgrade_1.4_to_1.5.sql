7c94b629b15ee9887ddc35763fc46ccd7885dae9
victoralagwu <victor@victor>
2018-05-02 18:07:42 +0100

    ACHECKER-3:Change all '0000-00-00 00:00:00' values to NULL: editing achecker upgrade script

--- install/db/achecker_upgrade_1.4_to_1.5.sql
+++ install/db/achecker_upgrade_1.4_to_1.5.sql
@@ -15,8 +15,6 @@ ALTER TABLE `language_text` MODIFY `revised_date` datetime default NULL;
 
 UPDATE `checks` SET create_date = NULL WHERE create_date = '0000-00-00 00:00:00';
 
-UPDATE `themes` SET last_updated = NULL WHERE last_updated = '0000-00-00 00:00:00';
-
 UPDATE `language_text` SET revised_date = NULL WHERE revised_date = '0000-00-00 00:00:00';
 
 
