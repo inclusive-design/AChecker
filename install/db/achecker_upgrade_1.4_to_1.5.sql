790ad415960b65fe9cddb3a0b38b57c430363c19
victoralagwu <victoralagwu@gmail.com>
2018-05-08 11:54:49 +0100

    ACHECKER-3: FIX:-Modify the themes table and change value of last_updated to NOT NULL

--- install/db/achecker_upgrade_1.4_to_1.5.sql
+++ install/db/achecker_upgrade_1.4_to_1.5.sql
@@ -11,6 +11,8 @@ ALTER TABLE `checks` MODIFY `create_date` datetime DEFAULT NULL;
 
 ALTER TABLE `language_text` MODIFY `revised_date` datetime default NULL;
 
+ALTER TABLE `themes` MODIFY `last_updated` date NOT NULL;
+
 UPDATE `checks` SET create_date = NULL WHERE create_date = '0000-00-00 00:00:00';
 
 UPDATE `language_text` SET revised_date = NULL WHERE revised_date = '0000-00-00 00:00:00';
