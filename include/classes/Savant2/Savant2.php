afd579fcfdc0707a11dcb2b35c456e894188a535
victoralagwu <victoralagwu@gmail.com>
2018-06-27 19:49:54 +0100

    ACHECKER-5:Patch for Savant

--- include/classes/Savant2/Savant2.php
+++ include/classes/Savant2/Savant2.php
@@ -994,51 +994,44 @@ class Savant2 {
 	* @throws object An error object with a SAVANT2_ERROR_ASSIGN code.
 	* 
 	*/
-	
-	function assign()
+	public function assign()
 	{
-		// this method is overloaded.
-		$arg = func_get_args();
+		// get the arguments; there may be 1 or 2.
+		$firstArg = @func_get_arg(0);
+		$secArg = @func_get_arg(1);
 		
-		// must have at least one argument. no error, just do nothing.
-		if (! isset($arg[0])) {
-			return;
-		}
-		
-		// assign by object
-		if (is_object($arg[0])) {
+		// assign from object
+		if (is_object($firstArg)) {
 			// assign public properties
-			foreach (get_object_vars($arg[0]) as $key => $val) {
-				if (substr($key, 0, 1) != '_') {
+			foreach (get_object_vars($firstArg) as $key => $val) {
+				// can't assign to __config
+				if ($key != '__config') {
 					$this->$key = $val;
 				}
 			}
-			return;
+			return true;
 		}
 		
-		// assign by associative array
-		if (is_array($arg[0])) {
-			foreach ($arg[0] as $key => $val) {
-				if (substr($key, 0, 1) != '_') {
+		// assign from associative array
+		if (is_array($firstArg)) {
+			foreach ($firstArg as $key => $val) {
+				// can't assign to __config
+				if ($key != '__config') {
 					$this->$key = $val;
 				}
 			}
-			return;
+			return true;
 		}
 		
-		// assign by string name and mixed value.
-		// 
-		// we use array_key_exists() instead of isset() becuase isset()
-		// fails if the value is set to null.
-		if (is_string($arg[0]) &&
-			substr($arg[0], 0, 1) != '_' &&
-			array_key_exists(1, $arg)) {
-			$this->$arg[0] = $arg[1];
-		} else {
-			return $this->error(SAVANT2_ERROR_ASSIGN, $arg);
+		// assign by name and value (can't assign to __config).
+		if (is_string($firstArg) && func_num_args() > 1 && $firstArg != '__config') {
+			$this->$firstArg = $secArg;
+			return true;
 		}
+		
+		// $firstArg was not object, array, or string.
+		return false;
 	}
-	
 	
 	/**
 	* 
