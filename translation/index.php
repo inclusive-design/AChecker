a761aeef374b0ccf20e0fd40e9cd340b38ce8d31
victoralagwu <victoralagwu@gmail.com>
2018-07-18 12:10:38 +0100

    ACHECKER-10

--- translation/index.php
+++ translation/index.php
@@ -31,42 +31,42 @@ if (isset($_REQUEST['submit']) || isset($_REQUEST['search']))
 	if (isset($_REQUEST['submit']))
 	{
 		if (isset($_REQUEST['term_type']) && $_REQUEST['term_type'] <> '') $term_type = $_REQUEST['term_type'];
-		
-		$sql = "SELECT * FROM ".TABLE_PREFIX."language_text 
+
+		$sql = "SELECT * FROM ".TABLE_PREFIX."language_text
 						WHERE language_code='".DEFAULT_LANGUAGE_CODE."'";
-		
+
 		if ($term_type <> '') $sql .= " AND variable = '".$term_type."'";
-		
+
 		if (isset($_REQUEST['new_or_translated']) && ($_REQUEST['new_or_translated'] == 1 || $_REQUEST['new_or_translated'] == 2))
 		{
 			$subquery = "(SELECT term FROM ".TABLE_PREFIX."language_text
 										WHERE language_code='".$_REQUEST['lang_code']."'
 										  AND text <> '')";
-			
+
 			if ($_REQUEST['new_or_translated'] == 1) $sql .= " AND term NOT IN ".$subquery;
 			if ($_REQUEST['new_or_translated'] == 2) $sql .= " AND term IN ".$subquery;
 		}
-		
+
 		if (isset($_REQUEST['new_or_translated']) && $_REQUEST['new_or_translated'] == 3)
 		{
-			$sql = "select * from ".TABLE_PREFIX."language_text a 
-							where language_code='".DEFAULT_LANGUAGE_CODE."' 
-								and exists (select 1 from ".TABLE_PREFIX."language_text b 
-														where language_code = '".$_REQUEST['lang_code']."' 
-															and a.term = b.term 
+			$sql = "select * from ".TABLE_PREFIX."language_text a
+							where language_code='".DEFAULT_LANGUAGE_CODE."'
+								and exists (select 1 from ".TABLE_PREFIX."language_text b
+														where language_code = '".$_REQUEST['lang_code']."'
+															and a.term = b.term
 															and a.revised_date > b.revised_date)";
 		}
 	}
-	
+
 	if (isset($_REQUEST['search']))
 	{
-		$sql = "SELECT * FROM ".TABLE_PREFIX."language_text 
+		$sql = "SELECT * FROM ".TABLE_PREFIX."language_text
 						WHERE language_code='".DEFAULT_LANGUAGE_CODE."'
 						  AND lower(term) like '%".$dao->addSlashes(strtolower(trim($_REQUEST['search_phase'])))."%'";
 	}
-	
+
 	$rows = $dao->execute($sql);
-	
+
 	if (is_array($rows)) $num_results = count($rows);
 	else $num_results = 0;
 }
@@ -83,7 +83,7 @@ if (isset($_REQUEST["save"]))
 	}
 }
 
-$rows_lang = $languagesDAO->getAll();				
+$rows_lang = $languagesDAO->getAll();
 
 include(AC_INCLUDE_PATH.'header.inc.php');
 
@@ -94,8 +94,8 @@ include(AC_INCLUDE_PATH.'header.inc.php');
 		<div class="row">
 			<div class="required" title="<?php echo _AC('required_field'); ?>">*</div>
 			<label for="lang_code"><?php echo _AC('choose_lang'); ?></label>:
-			<select name="lang_code" id="lang_code"> 
-<?php 
+			<select name="lang_code" id="lang_code">
+<?php
 	foreach ($rows_lang as $row_lang)
 	{
 ?>
@@ -117,20 +117,20 @@ include(AC_INCLUDE_PATH.'header.inc.php');
 
 		<div>
 			<?php echo _AC('new_or_translated'); ?><br />
-			<input type="radio" name="new_or_translated" value="0" id="u0" <?php if (!isset($_REQUEST['new_or_translated']) || $_REQUEST['new_or_translated'] == 0) { echo 'checked="checked"'; } ?> /><label for="u0"><?php echo _AC('all'); ?></label> 
-			<input type="radio" name="new_or_translated" value="1" id="u1" <?php if ($_REQUEST['new_or_translated'] == 1) { echo 'checked="checked"'; } ?> /><label for="u1"><?php echo _AC('new_terms'); ?></label> 
-			<input type="radio" name="new_or_translated" value="2" id="u2" <?php if ($_REQUEST['new_or_translated'] == 2) { echo 'checked="checked"'; } ?> /><label for="u2"><?php echo _AC('translated_terms'); ?></label> 
-			<input type="radio" name="new_or_translated" value="3" id="u3" <?php if ($_REQUEST['new_or_translated'] == 3) { echo 'checked="checked"'; } ?> /><label for="u3"><?php echo _AC('updated_terms'); ?></label> 
+			<input type="radio" name="new_or_translated" value="0" id="u0" <?php if (!isset($_REQUEST['new_or_translated']) || $_REQUEST['new_or_translated'] == 0) { echo 'checked="checked"'; } ?> /><label for="u0"><?php echo _AC('all'); ?></label>
+			<input type="radio" name="new_or_translated" value="1" id="u1" <?php if ($_REQUEST['new_or_translated'] == 1) { echo 'checked="checked"'; } ?> /><label for="u1"><?php echo _AC('new_terms'); ?></label>
+			<input type="radio" name="new_or_translated" value="2" id="u2" <?php if ($_REQUEST['new_or_translated'] == 2) { echo 'checked="checked"'; } ?> /><label for="u2"><?php echo _AC('translated_terms'); ?></label>
+			<input type="radio" name="new_or_translated" value="3" id="u3" <?php if ($_REQUEST['new_or_translated'] == 3) { echo 'checked="checked"'; } ?> /><label for="u3"><?php echo _AC('updated_terms'); ?></label>
 		</div>
 
 		<div>
 			<?php echo _AC('term_type'); ?><br />
-			<input type="radio" name="term_type" value="" id="t0" <?php if (!isset($_REQUEST['term_type']) || $_REQUEST['term_type'] == "") { echo 'checked="checked"'; } ?> /><label for="t0"><?php echo _AC('all'); ?></label> 
-			<input type="radio" name="term_type" value="_template" id="t1" <?php if ($_REQUEST['term_type'] == "_template") { echo 'checked="checked"'; } ?> /><label for="t1"><?php echo _AC('interface_terms'); ?></label> 
-			<input type="radio" name="term_type" value="_msgs" id="t2" <?php if ($_REQUEST['term_type'] == "_msgs") { echo 'checked="checked"'; } ?> /><label for="t2"><?php echo _AC('msg_terms'); ?></label> 
-			<input type="radio" name="term_type" value="_check" id="t3" <?php if ($_REQUEST['term_type'] == "_check") { echo 'checked="checked"'; } ?> /><label for="t3"><?php echo _AC('check_terms'); ?></label> 
-			<input type="radio" name="term_type" value="_guideline" id="t4" <?php if ($_REQUEST['term_type'] == "_guideline") { echo 'checked="checked"'; } ?> /><label for="t4"><?php echo _AC('guideline_terms'); ?></label> 
-			<input type="radio" name="term_type" value="_test" id="t5" <?php if ($_REQUEST['term_type'] == "_test") { echo 'checked="checked"'; } ?> /><label for="t5"><?php echo _AC('test_terms'); ?></label> 
+			<input type="radio" name="term_type" value="" id="t0" <?php if (!isset($_REQUEST['term_type']) || $_REQUEST['term_type'] == "") { echo 'checked="checked"'; } ?> /><label for="t0"><?php echo _AC('all'); ?></label>
+			<input type="radio" name="term_type" value="_template" id="t1" <?php if ($_REQUEST['term_type'] == "_template") { echo 'checked="checked"'; } ?> /><label for="t1"><?php echo _AC('interface_terms'); ?></label>
+			<input type="radio" name="term_type" value="_msgs" id="t2" <?php if ($_REQUEST['term_type'] == "_msgs") { echo 'checked="checked"'; } ?> /><label for="t2"><?php echo _AC('msg_terms'); ?></label>
+			<input type="radio" name="term_type" value="_check" id="t3" <?php if ($_REQUEST['term_type'] == "_check") { echo 'checked="checked"'; } ?> /><label for="t3"><?php echo _AC('check_terms'); ?></label>
+			<input type="radio" name="term_type" value="_guideline" id="t4" <?php if ($_REQUEST['term_type'] == "_guideline") { echo 'checked="checked"'; } ?> /><label for="t4"><?php echo _AC('guideline_terms'); ?></label>
+			<input type="radio" name="term_type" value="_test" id="t5" <?php if ($_REQUEST['term_type'] == "_test") { echo 'checked="checked"'; } ?> /><label for="t5"><?php echo _AC('test_terms'); ?></label>
 		</div>
 
 		<div>
@@ -144,19 +144,19 @@ include(AC_INCLUDE_PATH.'header.inc.php');
 		</div>
 
 		<div>
-			<input size="60" type="text" name="search_phase" id="search_phase" value="<?php echo htmlspecialchars(stripslashes($_REQUEST['search_phase'])); ?>" /> 
+			<input size="60" type="text" name="search_phase" id="search_phase" value="<?php echo htmlspecialchars(stripslashes($_REQUEST['search_phase'])); ?>" />
 		</div>
 
 		<div>
-			<input type="submit" name="search" value="<?php echo _AC('search_phase'); ?>" class="submit" /> 
+			<input type="submit" name="search" value="<?php echo _AC('search_phase'); ?>" class="submit" />
 		</div>
 	</fieldset>
-	
+
 	</div>
 </form>
 
-<?php 
-if (isset($_REQUEST['selected_term'])) 
+<?php
+if (isset($_REQUEST['selected_term']))
 {
 	$sql_english	= "SELECT * FROM ".TABLE_PREFIX."language_text WHERE language_code='".DEFAULT_LANGUAGE_CODE."' AND term='".$_REQUEST["selected_term"]."'";
 	if ($_REQUEST["term_type"] <> "") $sql_english .= " AND variable='".$_REQUEST["term_type"]."' ";
@@ -165,7 +165,7 @@ if (isset($_REQUEST['selected_term']))
 	$row_english = $rows_english[0];
 
 	$rows_selected = $languageTextDAO->getByTermAndLang($_REQUEST["selected_term"], $_REQUEST["lang_code"]);
-	
+
 function trans_form() {
 	global $row_english, $rows_selected;
 	global $langs;
@@ -243,46 +243,41 @@ if ($num_results > 0)
 		if (isset($_REQUEST["search"]))
 			$submits .= htmlspecialchars(SEP)."search=1";
 
-		foreach ($rows as $row) 
+		foreach ($rows as $row)
 		{
 			if ($row['term'] == $_REQUEST["selected_term"])
 				echo '<li>'."\n".'<a name="anchor" title="anchor"></a>'."\n";
 			else
 				echo '<li>'."\n";
-			
+
 				echo '<a href="'.$_SERVER['PHP_SELF'].'?selected_term='.$row['term'].htmlspecialchars(SEP).'lang_code='.$_REQUEST['lang_code'].htmlspecialchars(SEP).'new_or_translated='.$_REQUEST["new_or_translated"].htmlspecialchars(SEP).'term_type='.$_REQUEST["term_type"].htmlspecialchars(SEP).'search_phase='.$_REQUEST["search_phase"].$submits.'#anchor" ';
 				if ($row['term'] == $_REQUEST["selected_term"]) echo 'class="selected"';
 				echo '>';
 				echo $row['term'];
 				echo '</a>'."\n";
 
-	
+
 			// display if the term is new or translated
 			$rows_check = $languageTextDAO->getByTermAndLang($row['term'], $_REQUEST['lang_code']);
 			$row_check = $rows_check[0];
-			
+
 			// check if the term is new
 			if ($row_check['text'] == '')
 				echo '&nbsp;<small>*New*</small>'."\n";
-			
+
 			// compare revised_date to see if the term is updated since last translation
 			if ($row_check['revised_date'] <> '' && $row['revised_date'] > $row_check['revised_date'])
 				echo '&nbsp;<small>*Updated*</small>'."\n";
-				
+
 			echo '<br /><br/>';
 			// display translation form
 			if ($row['term'] == $_REQUEST["selected_term"]) trans_form();
-			
+
 			echo '</li>'."\n";
 		}
 	}
 	echo '</ul>'."\n";
 	echo '</div>'."\n";
 }
-ob_clean();
-$mpdf = new \Mpdf\Mpdf();
-$mpdf->WriteHTML('<h1>Hello worqeq!</h1>');
-$mpdf->Output();
-ob_end_clean();
-include(AC_INCLUDE_PATH.'footer.inc.php'); 
+include(AC_INCLUDE_PATH.'footer.inc.php');
 ?>
